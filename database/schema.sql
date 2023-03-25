1d472f2f6a3457a7f15134b8d42f046c240fee3a
Amro Khaled <wshcylinder@gmail.com>
2023-03-25 15:25:19 +0200

    :zap: Include DB Schema and seeeder

---
+++ database/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `price` int NOT NULL,
+  `type` ENUM("dvd","book","furniture") NOT NULL,
+  `attributes` json DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sku` (`sku`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
